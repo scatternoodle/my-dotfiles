8c49d515bee31c0788169c1beff03b8c411a683f
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2025-06-25 23:52:42 +0100

    remove 'brewfullup' alias

--- bashrc/.config/bashrc-exports/sources/misc.sh
+++ bashrc/.config/bashrc-exports/sources/misc.sh
@@ -3,7 +3,6 @@ export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
 
 alias zen='flatpak run app.zen_browser.zen -P & disown'
 
-alias brewfullup='brew update && brew outdated && brew upgrade && brew cleanup'
 function sysupdate() {
 	sudo apt update && DEBIAN_FRONTEND=noninteractive sudo apt upgrade -y && \
 		sudo apt autoremove -y && \
