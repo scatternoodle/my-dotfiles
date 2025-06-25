7b9bef2d76973ae1c6ed359e8c6da4e86199d38c
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2025-06-25 23:58:58 +0100

    bashrc - move install script to its own source file

--- bashrc/.config/bashrc-exports/sources/misc.sh
+++ bashrc/.config/bashrc-exports/sources/misc.sh
@@ -3,10 +3,3 @@ export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
 
 alias zen='flatpak run app.zen_browser.zen -P & disown'
 
-function sysupdate() {
-	sudo apt update && DEBIAN_FRONTEND=noninteractive sudo apt upgrade -y && \
-		sudo apt autoremove -y && \
-		flatpak update --noninteractive && \
-		brew update && brew upgrade && brew cleanup && \
-		cargo install-update --all
-}
---
+++ bashrc/.config/bashrc-exports/sources/update.sh
@@ -0,0 +1,8 @@
+function sysupdate() {
+	sudo apt update && DEBIAN_FRONTEND=noninteractive sudo apt upgrade -y && \
+		sudo apt autoremove -y && \
+		sudo snap refresh && \
+		flatpak update --noninteractive && \
+		brew update && brew upgrade && brew cleanup && \
+		cargo install-update --all
+}
