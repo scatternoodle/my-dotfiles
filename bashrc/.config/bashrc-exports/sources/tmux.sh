c07a4fe86b28a29d767d6e235b95f0adebf622fd
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2025-07-02 10:55:03 +0100

    bashrc: tmux: improve tmn alias - now just opens a session in the specified directory, does not open neovim by default.

--- bashrc/.config/bashrc-exports/sources/tmux.sh
+++ bashrc/.config/bashrc-exports/sources/tmux.sh
@@ -3,8 +3,7 @@
 # Other scripts in this repo use tmux incidentally for other purposes, but
 # this script is reserved for commands focused on using/configuring tmux and
 # orchestrating sessions.
-tmn() { tmux new-session -c "${1}" -s "${2}" 'nvim' ';' new-window; }
-tmk() { tmux kill-session -t "${1}"; }
+tmn() { tmux new-session -c "${1}" -s "${2}"; }
 alias tmks='tmux kill-server'
 alias tml='tmux ls'
 alias tma='tmux a'
