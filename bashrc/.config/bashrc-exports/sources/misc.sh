605250ab48ad5b85993ec7371e25338c65a0493d
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2025-08-12 15:02:14 +0100

    add fix_downloads() function for when I accidentally rm -f my Downloads folder.

--- bashrc/.config/bashrc-exports/sources/misc.sh
+++ bashrc/.config/bashrc-exports/sources/misc.sh
@@ -4,3 +4,11 @@ export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
 alias zen='flatpak run app.zen_browser.zen -P & disown'
 alias dotfiles='tmux new-session -s dotfiles -c "${HOME}/dotfiles"'
 
+function fix_downloads() {
+	mkdir -p ~/Downloads && \
+	chown "$USER":"$USER" ~/Downloads && \
+	chmod 700 ~/Downloads && \
+	xdg-user-dirs-update && \
+	xdg-user-dir DOWNLOAD
+}
+
