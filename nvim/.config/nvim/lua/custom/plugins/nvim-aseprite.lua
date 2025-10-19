d3d50bbe0e43086c63a92a11c63fb0b9d45b1d9c
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2025-10-19 14:40:47 +0100

    aseprite keybinds

--- nvim/.config/nvim/lua/custom/plugins/nvim-aseprite.lua
+++ nvim/.config/nvim/lua/custom/plugins/nvim-aseprite.lua
@@ -5,5 +5,11 @@ return {
 	-- Uncomment to use local dev version
 	dir = vim.fn.expand("$HOME/workspace/github.com/scatternoodle/nvim-aseprite"),
 	name = "nvim-aseprite",
+	event = "VeryLazy",
 	opts = {},
+	keys = {
+		{ "<leader>a", desc = "Aseprite" },
+		{ "<leader>ar", "<cmd>AsepriteRestart<cr>", desc = "Restart Aseprite" },
+		{ "<leader>as", "<cmd>AsepriteStop<cr>", desc = "Stop Aseprite" },
+	},
 }
