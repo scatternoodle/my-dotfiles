10d450151760e04fa1c2fc600153340b50011b22
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2025-10-22 13:24:33 +0100

    change obsidian vaults default path

--- bashrc/.config/bashrc-exports/sources/obsidian.sh
+++ bashrc/.config/bashrc-exports/sources/obsidian.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export OBSIDIAN_VAULTS="$HOME/obsidian/vaults"
+export OBSIDIAN_VAULTS="$HOME/obsidian"
 alias 'ov'="cd $OBSIDIAN_VAULTS"
 alias 'ob'="cd $OBSIDIAN_VAULTS/brain"
